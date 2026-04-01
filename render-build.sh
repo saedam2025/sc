0279f803686ab1dd486d057e609fa474beec33ff
saedam2025 <windows7@hanmail.net>
2026-04-01 16:11:06 +0900

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+# 오류 발생 시 즉시 중단
 set -o errexit
+
+# 1. 라이브러리 설치
 pip install -r requirements.txt
+
+# 2. PDF 엔진 wkhtmltopdf 설치
 apt-get update && apt-get install -y wkhtmltopdf
